efa5591fbb368db22e985bc611edd840643aa3a7
Ephraim-Mago <ephraimmago@gmail.com>
2022-04-13 21:17:14 +0100

    Update website

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,3 @@
-$(function() {
-    $('form').submit(function() {
-        $('#result').text(JSON.stringify($('form').serializeObject()));
-        return false;
-    });
-});
-
 var $form = $('form#form-enga'),
     url = 'https://script.google.com/macros/s/AKfycbywdSuSP1bENuiW_eL8WeTiVsNPZjGMhlVPlbHB5Jy17jaZPQ4iiRE6pSt7bmgeDvxQGQ/exec';
 
